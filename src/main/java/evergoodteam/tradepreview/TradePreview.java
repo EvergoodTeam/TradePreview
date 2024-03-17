49adaf9d3dd2a2a37562d02bc22ccc655ce322e2
Libra <73041325+Librv@users.noreply.github.com>
2024-03-17 14:30:59 +0100

    Move client callbacks to TradePreviewClient

--- src/main/java/evergoodteam/tradepreview/TradePreview.java
+++ src/main/java/evergoodteam/tradepreview/TradePreview.java
@@ -1,12 +1,9 @@
 package evergoodteam.tradepreview;
 
 import evergoodteam.chassis.client.gui.text.GradientText;
-import evergoodteam.chassis.config.option.AbstractOption;
 import evergoodteam.chassis.config.option.CategoryOption;
 import evergoodteam.chassis.config.option.ColorOption;
 import evergoodteam.chassis.config.option.IntegerSliderOption;
-import evergoodteam.chassis.util.gui.ColorUtils;
-import evergoodteam.tradepreview.client.TradePreviewClient;
 import evergoodteam.tradepreview.client.gui.TradePreviewScreenTexts;
 import evergoodteam.tradepreview.networking.TradePreviewNetworking;
 import evergoodteam.tradepreview.utils.Reference;
@@ -14,8 +11,6 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.function.Consumer;
 
 import static evergoodteam.tradepreview.utils.Reference.CONFIGS;
 
@@ -25,14 +20,12 @@ public class TradePreview implements ModInitializer {
     public static final TradePreviewNetworking NETWORKING = new TradePreviewNetworking();
     public static final CategoryOption POSITION = new CategoryOption(CONFIGS, "position", "Overlay's coordinates on the screen");
     public static final CategoryOption COLORS = new CategoryOption(CONFIGS, "colors", "Overlay's colours");
-    public static final IntegerSliderOption X_COORD = new IntegerSliderOption("xCoordinate", 0, 3840, 0).getBuilder()
-            .addUpdateCallback(getIntCallback(newVal -> TradePreviewClient.offersWidget.x = newVal)).build();
-    public static final IntegerSliderOption Y_COORD = new IntegerSliderOption("yCoordinate", 0, 2160, 0).getBuilder()
-            .addUpdateCallback(getIntCallback(newVal -> TradePreviewClient.offersWidget.y = newVal)).build();
-    public static final ColorOption BACKGROUND = new ColorOption(getStringCallback(newVal -> TradePreviewClient.offersWidget.backgroundColor = newVal), "backgroundColor", "6D272B45");
-    public static final ColorOption OUTLINE = new ColorOption(getStringCallback(newVal -> TradePreviewClient.offersWidget.outlineColor = newVal), "outlineColor", "6D272B45");
-    public static final ColorOption GLINT_BACK = new ColorOption(getStringCallback(newVal -> TradePreviewClient.offersWidget.glintBack = newVal), "glintBack", "6D55FFFF");
-    public static final ColorOption GLINT_OUT = new ColorOption(getStringCallback(newVal -> TradePreviewClient.offersWidget.glintOut = newVal), "glintOut", "6D55FFFF");
+    public static final IntegerSliderOption X_COORD = new IntegerSliderOption("xCoordinate", 0, 3840, 0);
+    public static final IntegerSliderOption Y_COORD = new IntegerSliderOption("yCoordinate", 0, 2160, 0);
+    public static final ColorOption BACKGROUND = new ColorOption("backgroundColor", "6D272B45");
+    public static final ColorOption OUTLINE = new ColorOption("outlineColor", "6D272B45");
+    public static final ColorOption GLINT_BACK = new ColorOption("glintBack", "6D55FFFF");
+    public static final ColorOption GLINT_OUT = new ColorOption("glintOut", "6D55FFFF");
 
     @Override
     public void onInitialize() {
@@ -41,24 +34,6 @@ public class TradePreview implements ModInitializer {
         initConfig();
 
         NETWORKING.registerSyncServerReceiver();
-    }
-
-    private static AbstractOption.OptionUpdateCallback<Integer> getIntCallback(Consumer<Integer> consumer) {
-        return new AbstractOption.OptionUpdateCallback<>() {
-            @Override
-            public void onAnyUpdate(Integer newValue) {
-                consumer.accept(newValue);
-            }
-        };
-    }
-
-    private static AbstractOption.OptionUpdateCallback<String> getStringCallback(Consumer<Integer> consumer) {
-        return new AbstractOption.OptionUpdateCallback<>() {
-            @Override
-            public void onAnyUpdate(String newValue) {
-                consumer.accept(ColorUtils.ARGB.getIntFromHexARGB(newValue));
-            }
-        };
     }
 
     public void initConfig() {
